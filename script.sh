02a584e24b1dbf8ca8952e989646075a96a76041
Jesus-0sorio <jaoz10.16@gmail.com>
2024-08-06 15:41:59 -0500

    chore: Remove commented out code and unnecessary newline in script.sh

--- script.sh
+++ script.sh
@@ -32,22 +32,3 @@ echo "Installing Consul"
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install consul -y	
-
-# echo "Installing NodeJS"
-
-# # Install NodeJS
-# sudo apt install -y ca-certificates curl gnupg
-# sudo mkdir -p /etc/apt/keyrings
-# curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-
-# # Add NodeJS repository
-# NODE_MAJOR=20
-# echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-
-# # Update package index
-# sudo apt update
-
-# # Install NodeJS
-# sudo apt install -y nodejs
-# cd /home/vagrant/microProducts
-# npm install
